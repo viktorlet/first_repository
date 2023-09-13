b1771192b9db65236bf40011a99f2854daa5070f
Victor Letunovskiy <viktorlet@list.ru>
2023-09-13 11:55:46 +0300

    Add eq, lt, le in class Person

--- main.py
+++ main.py
@@ -9,8 +9,18 @@ class Person:
     def __str__(self):
         return f'{self.name}: age {self.age}, city {self.city }'
 
+    def __eq__(self, other):
+        return self.age == other.age
+
+    def __lt__(self, other):
+        return self.age < other.age
+
+    def __le__(self, other):
+        return self.age <= other.age
+
+
 
 mike = Person('Mike', 32)
 nick = Person('Nick', 28, 'Saint-Petersburg')
-print(mike)
-hello(nick)
+
+
