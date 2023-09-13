85465b7503f83415268d1bc40a5f8a35f96b2c1b
Victor Letunovskiy <viktorlet@list.ru>
2023-09-13 10:12:38 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age, city='Moscow'):
+        self.name = name
+        self.age = age
+        self.city = city
+
+    def __str__(self):
+        return f'{self.name}: age {self.age}, city {self.city }'
+
+
+mike = Person('Mike', 32)
+print(mike)
